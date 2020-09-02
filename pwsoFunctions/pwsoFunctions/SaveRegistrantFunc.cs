a9f710890d54ade4bbcc68add2f696ea124d1e37
Van <lvanlowe@inrvisions.com>
2020-09-02 10:04:58 -0400

    add classes for registrants

--- pwsoFunctions/pwsoFunctions/SaveRegistrantFunc.cs
+++ pwsoFunctions/pwsoFunctions/SaveRegistrantFunc.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
@@ -12,5 +13,56 @@ namespace pwsoFunctions
         {
             log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
         }
+    }
+
+    public class RegistrantMessage
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string nickName { get; set; }
+        public string size { get; set; }
+        public int sportId { get; set; }
+        public int programId { get; set; }
+        public string email1 { get; set; }
+        public string email2 { get; set; }
+        public string email3 { get; set; }
+        public string phone1 { get; set; }
+        public string phone2 { get; set; }
+        public string phone3 { get; set; }
+        public string phoneType1 { get; set; }
+        public string phoneType2 { get; set; }
+        public string phoneType3 { get; set; }
+        public bool canText1 { get; set; }
+        public bool canText2 { get; set; }
+        public bool canText3 { get; set; }
+        public string sportName { get; set; }
+        public string programName { get; set; }
+        public bool isVolunteer { get; set; }
+        public bool isWaitlisted { get; set; }
+    }
+
+    public class RegistrantDb
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string nickName { get; set; }
+        public string size { get; set; }
+        public int sportId { get; set; }
+        public int programId { get; set; }
+        public List<string> Emails { get; set; }
+        public List<RegistrantPhone> Phones{ get; set; }
+        public string sportName { get; set; }
+        public string programName { get; set; }
+        public bool isVolunteer { get; set; }
+        public bool isWaitlisted { get; set; }
+    }
+
+    public class RegistrantPhone
+    {
+        public string Phone { get; set; }
+        public string PhoneType { get; set; }
+        public bool CanText { get; set; }
     }
 }
