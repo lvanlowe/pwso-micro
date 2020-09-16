0724398e40e0707522168c679ade72bcf71f7304
Van <lvanlowe@inrvisions.com>
2020-09-16 10:00:32 -0400

    add sender from client

--- pwsoFunctions/pwsoFunctions/SaveRegistrantFunc.cs
+++ pwsoFunctions/pwsoFunctions/SaveRegistrantFunc.cs
@@ -95,29 +95,7 @@ namespace pwsoFunctions
         public string ProgramName { get; set; }
         public bool IsVolunteer { get; set; }
         public bool IsWaitListed { get; set; }
+        public string Sender { get; set; }
     }
 
-    //public class RegistrantDb
-    //{
-    //    public int Id { get; set; }
-    //    public string FirstName { get; set; }
-    //    public string LastName { get; set; }
-    //    public string NickName { get; set; }
-    //    public string Size { get; set; }
-    //    public int SportId { get; set; }
-    //    public int ProgramId { get; set; }
-    //    public List<string> Emails { get; set; }
-    //    public List<RegistrantPhone> Phones{ get; set; }
-    //    public string Sport { get; set; }
-    //    public string ProgramName { get; set; }
-    //    public bool IsVolunteer { get; set; }
-    //    public bool IsWaitListed { get; set; }
-    //}
-
-    //public class RegistrantPhone
-    //{
-    //    public string Phone { get; set; }
-    //    public string PhoneType { get; set; }
-    //    public bool CanText { get; set; }
-    //}
 }
