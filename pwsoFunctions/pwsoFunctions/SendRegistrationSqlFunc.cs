44a5dd80522011845ff029dbb6be409d985e1b5e
Van <lvanlowe@inrvisions.com>
2020-09-23 11:03:38 -0400

    add registrant sql function

---
+++ pwsoFunctions/pwsoFunctions/SendRegistrationSqlFunc.cs
@@ -0,0 +1,48 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using InformationService.Interfaces;
+using InformationService.Models;
+using InformationService.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using JsonSerializer = System.Text.Json.JsonSerializer;
+using pwsoProcesses.Models;
+
+namespace pwsoFunctions
+{
+    public static class SendRegistrationSqlFunc
+    {
+        [FunctionName("SendRegistrationSqlFunc")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            try
+            {
+                string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+                var registrantDb = JsonSerializer.Deserialize<RegistrantDb>(requestBody);
+                var connectionString = System.Environment.GetEnvironmentVariable("SQLAZURECONNSTR_TrainingModel");
+                var options = new DbContextOptionsBuilder<PwsodbContext>().UseSqlServer(connectionString ?? throw new InvalidOperationException()).Options;
+                var context = new PwsodbContext(options);
+                ITrainingRepository trainingRepository = new TrainingRepository(context);
+
+            }
+            catch (Exception e)
+            {
+                log.LogInformation(e.ToString());
+                return new BadRequestResult();
+            }
+
+
+            return new OkResult();
+        }
+    }
+}
